1a0cbf108dab2c85200b04befdd8dc1f62208796
Luis Fernando Moreno Vega <Luis_RM2607@hotmail.com>
2018-08-30 23:06:14 -0500

    Error

--- Alumno.java
+++ Alumno.java
@@ -1,11 +1,15 @@
 public class Alumno
 {
-    private String nombre;
-    private int clave;
+   private String nombre;
+   private int clave;
     
-    public Alumno(String nombre, int clave)
-    {
-        this.nombre = nombre;
-        this.clave = clave;
-    }
+   public String dimeNombre()
+   {
+       return this.nombre;
+   }
+   
+   public int dimeClave()
+   {
+       return this.clave;
+   }
 }
--- Laboratorio.java
+++ Laboratorio.java
@@ -2,18 +2,37 @@ import java.util.ArrayList;
  
 public class Laboratorio
 {
-     private ArrayList<Alumno> alumnos;
+   private ArrayList<Alumno> alumnos;
      
+   public Laboratorio()
+   {
+       alumnos = new ArrayList<Alumno>();
+   }
      
-     public Laboratorio()
-     {
-         alumnos = new ArrayList<Alumno>();
-     }
-     
-     public void incribir(String nombre, int clave)
-     {
-         Alumno alumn = new Alumno(nombre, clave);
-         
-     } 
+   public void incribir(String nombre, int clave)
+   {
+       Alumno alumno = new Alumno(nombre, clave);  
+   } 
+    
+   public void darDeBaja(int claveDeBaja)
+   {
+       for(int i=0; i<alumnos.size(); i++)
+       {
+           Alumno alumn = alumnos.get(i);
+           if(alumn.dimeClave() == claveDeBaja)
+           {
+               alumnos.remove(i);
+           }
+       }
+   }
+   
+   public void imprimirLista()
+   {
+       for(int i=0; i<alumnos.size(); i++)
+       {
+           Alumno alumn = alumnos.get(i);
+           System.out.println("Alumno: "+ alumn.dimeNombre() + " Clave: "+ alumn.dimeClave());
+       }
+   }
 }
 
